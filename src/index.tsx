9816bc2e423994aa52b014505068e4813c4206df
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-10-09 15:37:21 +0100

    reconfigured index.tsx to consume browserRouter in app

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
 
